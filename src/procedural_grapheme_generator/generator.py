d6b47d91fb8e6edd82b49dd32b7402b696b964fa
Aravind-Sundararajan <Aravind.Sundararajan@protonmail.com>
2022-05-24 13:54:20 -0700

    thinking about nonisotropy and nontotalistic rules

--- src/procedural_grapheme_generator/generator.py
+++ src/procedural_grapheme_generator/generator.py
@@ -1,6 +1,8 @@
+from models import Board, Options
+
 class generator:
     def __init__(self):
         pass
 
-    def eval(self):
-        pass
+    def eval(self, board: Board, options):
+        board.eval()
--- src/procedural_grapheme_generator/models.py
+++ src/procedural_grapheme_generator/models.py
@@ -1,23 +1,53 @@
 from dataclasses import dataclass, field
+from enum import Enum
+from operator import truediv
+
+
+class _NONTOTALISTIC(Enum):
+    "c"
 
 
 @dataclass
-class board_base:
-    size_x: int = 1
-    size_y: int = 1
-    matrix: list = []
+class Rules_default:
+    born: list = [3]
+    survive: list = [2, 3]
+
+
+@dataclass
+class Rules_base:
+    born: list = [3]
+    survive: list = [2, 3]
+
+
+@dataclass
+class Rules(Rules_base, Rules_default):
+    pass
+
+
+@dataclass
+class Board_base:
+    size_x: int = 8
+    size_y: int = 8
+    matrix: list[list[int]] = field(
+        default_factory=lambda: [[0 for i in range(8)] for j in range(8)]
+    )
 
     def reshape(self, size_x, size_y):
         pass
 
-
-@dataclass
-class board_default:
-    size_x: int = 1
-    size_y: int = 1
-    matrix: list = []
+    def eval(self):
+        pass
 
 
 @dataclass
-class board(board_base, board_default):
+class Board_default:
+    size_x: int = 8
+    size_y: int = 8
+    matrix: list[list[int]] = field(
+        default_factory=lambda: [[0 for i in range(8)] for j in range(8)]
+    )
+
+
+@dataclass
+class Board(Board_base, Board_default):
     pass
